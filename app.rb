96068abb69a42715c9716fb0bb0af67f804f3f01
Ashwini <ashie.oh@gmail.com>
2017-02-28 13:36:54 +0000

    used a form (get and post)

--- app.rb
+++ app.rb
@@ -20,3 +20,19 @@ get '/cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
+end
+
+get '/form' do
+  erb :form
+end
+
+get '/named-cat/:name' do
+  p params
+  @name = params[:name]
+  erb :index
+end
